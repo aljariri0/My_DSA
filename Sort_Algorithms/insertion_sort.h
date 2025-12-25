f28639cabff577d47c86085b95616c0e70354bf7
Issa aljariri <issaaljariri0@gmail.com>
2025-12-25 14:48:42 +0300

    Add insertion sort class

---
+++ Sort_Algorithms/insertion_sort.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+class InsertionSort
+{
+
+public:
+    int *element;
+    int size;
+
+    InsertionSort(int arr[], int size)
+    {
+
+        this->size = size;
+        element = arr;
+    }
+
+    void Swap(int &a, int &b)
+    {
+
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    void Sort()
+    {
+
+        for (int i = 1; i < size; i++)
+        {
+            int j = i - 1;
+            int x = element[i];
+
+            while (j > -1 && element[j] > x)
+            {
+                element[j + 1] = element[j];
+                j--;
+            }
+            element[j + 1] = x;
+        }
+    }
+
+    void Display()
+    {
+
+        for (int i = 0; i < size; i++)
+        {
+            cout << element[i] << " ";
+        }
+
+        cout << "\n";
+    }
+};
