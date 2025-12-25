c39247fdb52a71b19cf3427d2e3442c5581e564a
Issa aljariri <issaaljariri0@gmail.com>
2025-12-25 14:47:45 +0300

    Add insertion sort class

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
-//#include "LinkedList.h"
-//#include "CircularLinkedList.h"
-//#include "DoublyLinkedList.h"
-//#include "CircularDoublyLinkedList.h"
-//#include "Stack.h"
-//#include "Queue.h"
+// #include "LinkedList.h"
+// #include "CircularLinkedList.h"
+// #include "DoublyLinkedList.h"
+// #include "CircularDoublyLinkedList.h"
+// #include "Stack.h"
+// #include "Queue.h"
 #include "clsTree.h"
 #include "Sort_Algorithms/bubble_sort.h"
-
+#include "Sort_Algorithms/insertion_sort.h"
 
 using namespace std;
 
-int main() {
+int main()
+{
+    int arr[] = {8, 5, 7, 3, 2};
+    InsertionSort s(arr, 5);
 
-
+    s.Display();
+    s.Sort();
+    s.Display();
 }
